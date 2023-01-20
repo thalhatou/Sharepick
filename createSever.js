76de48277022e45322d4dfb73748d2ec13559a56
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2023-01-20 21:22:47 +0100

    package.json added

--- createSever.js
+++ createSever.js
@@ -2,12 +2,20 @@ const http = require('http')
 const port = 8000
 const sever = http.createServer(
     (request, response) => {
-        console.log(request.url);
-        response.end('hello world')
-
+        if (request.url == '/about') 
+        response.end('about us page');
+        else if (request.url == '/login')
+            response.end('login page');
+        else if (request.url == 'logout')
+            response.end('logout page');
+     else {
+         response.writeHead(404)
+        response.end('page not found')
+            
+        }
 
     }
-)
+);
 
 sever.listen(port)
 console.log(`server is running on port ${port}`)
