73492a8427e6d35588c5720a48e3485177d8b473
Thalhatou Yahya <thalhatouyahya5352@gmail.com>
2023-01-21 20:09:48 +0100

    front ui added

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express')
+const path = require('path')
 const app = new express()
 const port = 8080
 
 app.get('/', (request, response) => {
-    response.send('Hello world')
+    response.sendFile(path.resolve(__dirname, 'index.html'));
 })
 
 app.get('/about', (req, res) => {
     res.send('This is the about us page')
 })
+
+
 
 
 app.listen(port, () => {
